c69b2df16738fdcee77d72530b9c424389856bb3
jeffheslopemi-cmyk <jeffheslopemi@gmail.com>
2026-03-24 00:38:24 +0300

    Update page.js

--- app/app/shows/page.js
+++ app/app/shows/page.js
@@ -1,15 +1,18 @@
 export default function Shows() {
   return (
-    <div style={{ padding: "30px", textAlign: "center" }}>
-      <h1>BTL TV Shows</h1>
+    <div style={{ padding: "40px" }}>
+      <h1>Shows</h1>
 
-      <div style={{ display: "flex", gap: "20px", flexWrap: "wrap", justifyContent: "center" }}>
-        
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/ysz5S6PUM-U"></iframe>
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+        gap: "20px",
+        marginTop: "20px"
+      }}>
 
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/jNQXAC9IVRw"></iframe>
-
-        <iframe width="300" height="200" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+        <iframe src="https://www.youtube.com/embed/ysz5S6PUM-U"></iframe>
+        <iframe src="https://www.youtube.com/embed/jNQXAC9IVRw"></iframe>
+        <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
 
       </div>
     </div>
