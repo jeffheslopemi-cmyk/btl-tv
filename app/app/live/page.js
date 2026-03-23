3771d4da3cee5a09b3e8bb8a0f95a10d050993fc
jeffheslopemi-cmyk <jeffheslopemi@gmail.com>
2026-03-24 00:32:38 +0300

    Update page.js

--- app/app/live/page.js
+++ app/app/live/page.js
@@ -1,6 +1,6 @@
 "use client";
-import Hls from "hls.js";
 import { useEffect, useRef } from "react";
+import Hls from "hls.js";
 
 export default function Live() {
   const videoRef = useRef(null);
@@ -14,9 +14,18 @@ export default function Live() {
   }, []);
 
   return (
-    <div style={{ textAlign: "center", padding: "30px" }}>
+    <div style={{ textAlign: "center", padding: "40px" }}>
       <h1>Live TV</h1>
-      <video ref={videoRef} controls style={{ width: "80%" }} />
+
+      <video
+        ref={videoRef}
+        controls
+        style={{
+          width: "80%",
+          maxWidth: "900px",
+          borderRadius: "20px",
+        }}
+      />
     </div>
   );
 }
