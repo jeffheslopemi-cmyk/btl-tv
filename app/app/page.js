321189639da322596e34caf790da7a81509abe05
jeffheslopemi-cmyk <jeffheslopemi@gmail.com>
2026-03-24 00:28:55 +0300

    Delete app/app/page.js

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>BTL TV Home ✅</h1>
-      <p>Site is working</p>
-    </div>
-  );
-}
