b5483a8b5faf278b1277d5ed692be7278dadc118
jeffheslopemi-cmyk <jeffheslopemi@gmail.com>
2026-03-24 00:18:48 +0300

    Create page.js

---
+++ app/app/shows/page.js
@@ -0,0 +1,19 @@
+export default function Shows() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Shows</h1>
+
+      <iframe
+        width="400"
+        height="250"
+        src="https://www.youtube.com/embed/ysz5S6PUM-U"
+      ></iframe>
+
+      <iframe
+        width="400"
+        height="250"
+        src="https://www.youtube.com/embed/jNQXAC9IVRw"
+      ></iframe>
+    </div>
+  );
+}
